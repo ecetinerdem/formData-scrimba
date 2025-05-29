0f2b2261c3b8a8ad7c9ec53ee078d2186735eb5e
Cetin Erdem <cetoerdem1@gmail.com>
2025-05-29 21:35:38 +0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -32,3 +32,41 @@ function App() {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+
+//New form handling down!
+
+/** 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  
+  function signUp(formData) {
+    const email = formData.get("email")
+    console.log(email)
+  }
+  
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form action={signUp}>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+        <br />
+        
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
+        <br />
+        
+        <button>Submit</button>
+        
+      </form>
+    </section>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+
+*/
